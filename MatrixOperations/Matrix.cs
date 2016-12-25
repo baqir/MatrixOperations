6de0e470e3d38f108bdd5fdbe556b0a682250cf1
Baqir Alvi <baalvi@microsoft.com>
2016-12-25 17:12:29 +0530

    Adding initialization alongwith testing

--- MatrixOperations/Matrix.cs
+++ MatrixOperations/Matrix.cs
@@ -1,16 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MatrixOperations
 {
     public class Matrix
     {
-		public Matrix()
-		{
+		private double[,] MatrixImpl;
 
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="matrixString">The string representation for the matrix</param>
+		public Matrix(string matrixString)
+		{
+			this.InitializeMatrix(matrixString);
+		}
+
+		/// <summary>
+		/// Display the given matrix
+		/// </summary>
+		public void Display()
+		{
+			for (int i = 0; i < MatrixImpl.GetLength(0); i++)
+			{
+				for (int j = 0; j < MatrixImpl.GetLength(1); j++)
+				{
+					Console.Write(MatrixImpl[i,j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		/// <summary>
+		/// Initialize the matrix from the string representation
+		/// </summary>
+		/// <param name="matrixString">The string representation for the matrix</param>
+		private void InitializeMatrix(string matrixString)
+		{
+			if (string.IsNullOrWhiteSpace(matrixString))
+			{
+				throw new Exception("Empty matrix");
+			}
+
+			// Remove last ';' from matrix string
+			matrixString = matrixString.EndsWith(";") ? matrixString.Substring(0, matrixString.Length - 1) : matrixString;
+
+			string[] matrixData = matrixString.Split(';');
+			int rows = matrixData.Length;
+			int columns = matrixData[0].Split(' ').Length;
+
+			this.MatrixImpl = new double[rows, columns];
+
+			int i = 0;
+			foreach (string row in matrixData)
+			{
+				string[] rowValues = row.Split(' ');
+				if (rowValues.Length != columns)
+				{
+					throw new Exception("Unequal number of columns in matrix");
+				}
+
+				int j = 0;
+				foreach (string value in rowValues)
+				{
+					MatrixImpl[i, j] = Double.Parse(value);
+					j++;
+				}
+
+				i++;
+			}
 		}
     }
 }
---
+++ MatrixOperationsTests/InitializationTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MatrixOperations;
+
+namespace MatrixOperationsTests
+{
+	[TestClass]
+	public class InitializationTests
+	{
+		[TestMethod]
+		public void TestSuccessfulInitialization()
+		{
+			Matrix matrix = new Matrix("1 2;3 4;");
+		}
+
+		[TestMethod]
+		public void TestInitializationWithEmptyString()
+		{
+			try
+			{
+				new Matrix(string.Empty);
+				Assert.Fail("Should have thrown exception");
+			}
+			catch (Exception e)
+			{
+				Assert.AreEqual("Empty matrix", e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void TestInitializationWithNullString()
+		{
+			try
+			{
+				new Matrix(null);
+				Assert.Fail("Should have thrown exception");
+			}
+			catch (Exception e)
+			{
+				Assert.AreEqual("Empty matrix", e.Message);
+			}
+		}
+
+		[TestMethod]
+		public void TestInitializationWithUnequalColumns()
+		{
+			try
+			{
+				new Matrix("1 2;3 4;5;");
+				Assert.Fail("Should have thrown exception");
+			}
+			catch (Exception e)
+			{
+				Assert.AreEqual("Unequal number of columns in matrix", e.Message);
+			}
+		}
+	}
+}
